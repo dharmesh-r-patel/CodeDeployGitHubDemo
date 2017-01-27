fde76ae1f9f829ad7b8b754d9362d2c94a864c7d
dharmesh <dharmesh@numbertank.com>
2017-01-27 11:02:53 +0530

    add app.js

--- instance-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt -y update
-sudo apt -y install awscli
-sudo apt -y install ruby
-sudo apt -y install httpd
-cd /home/ubuntu
-aws s3 cp s3://aws-codedeploy-ap-south-1/latest/install . --region ap-south-1
-chmod +x ./install
-./install auto
